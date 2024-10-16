acbd3f74a306e8b28290aacd2e0fff56c035dc58
MccFlurry <meencanta7u7@gmail.com>
2024-10-15 19:56:06 -0500

    v3

--- MapaRumania/MapaRumania.py
+++ MapaRumania/MapaRumania.py
@@ -34,30 +34,6 @@ romania_map = {
     'Neamt': [('Iasi', 87)],
 }
 
-# Función heurística (distancias en línea recta a Bucarest)
-heuristics = {
-    'Arad': 366,
-    'Bucharest': 0,
-    'Craiova': 160,
-    'Drobeta': 242,
-    'Eforie': 161,
-    'Fagaras': 176,
-    'Giurgiu': 77,
-    'Hirsova': 151,
-    'Iasi': 226,
-    'Lugoj': 244,
-    'Mehadia': 241,
-    'Neamt': 234,
-    'Oradea': 380,
-    'Pitesti': 100,
-    'Rimnicu Vilcea': 193,
-    'Sibiu': 253,
-    'Timisoara': 329,
-    'Urziceni': 80,
-    'Vaslui': 199,
-    'Zerind': 374,
-}
-
 # Función para cargar y mostrar la imagen del mapa
 def load_map_image():
     map_image = Image.open("recursos/rumania.png")
